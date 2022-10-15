ba1ba0e38215fe34b4699f1f0f8379477727f7c5
Layla Ventillari <laylaventilari5@gmail.com>
2022-10-14 21:52:43 -0300

    technologies

--- src/components/Technology/index.tsx
+++ src/components/Technology/index.tsx
@@ -1,10 +1,24 @@
-import { Container } from "./style";
+import { Container, ItemContainer, ItemsContainer, TitleContainer } from "./style";
 
 
 export function Technology(){
   return(
     <Container>
-      <span>Javascript</span>
+      <TitleContainer>
+        <h1>Tecnologias</h1>
+      </TitleContainer>
+      <ItemContainer>
+        <ItemsContainer><p>JavaScript</p></ItemsContainer>
+        <ItemsContainer><p>ReactJs</p></ItemsContainer>
+        <ItemsContainer><p>TypeScript</p></ItemsContainer>
+        <ItemsContainer><p>Angular</p></ItemsContainer>
+        <ItemsContainer><p>NodeJs</p></ItemsContainer>
+        <ItemsContainer><p>Python</p></ItemsContainer>
+        <ItemsContainer><p>Git</p></ItemsContainer>
+        <ItemsContainer><p>Github</p></ItemsContainer>
+        <ItemsContainer><p>Java</p></ItemsContainer>
+        </ItemContainer>
+      
     </Container>
   )
 }
--- src/components/Technology/style.ts
+++ src/components/Technology/style.ts
@@ -1,12 +1,46 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-background: ${(props) => props.theme['base-subtitle']};
+  background: ${(props) => props.theme['base-subtitle']};
   border-radius: 8px;
   overflow: hidden;
-  width: 21.75rem;
-  height: 18rem;
+  width: 346px;
+  height: 208px;
   margin-left: 40px;
   gap: 2rem;
   margin-top: 2rem;
 `
+export  const ItemContainer = styled.div` 
+  display: grid;
+  column-gap: 15px;
+  row-gap: 15px;
+  grid: repeat(3, 16px) / auto-flow 84px;
+  margin-left: 30px;
+
+`
+export  const TitleContainer = styled.div`
+h1{
+  font-family: 'Baloo 2';
+  color: ${(props) => props.theme['base-label']};
+  padding: 35px;
+  margin-left: 30px;
+  font-size: 20px;
+}
+`
+export  const ItemsContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 90px;
+height: 24px;
+background: ${(props) => props.theme['base-label']};
+border-radius: 30px;
+p 
+{
+  color: ${(props) => props.theme['base-title']};
+  font-size: 12px;
+}
+`
+
+
+
